a74515a5982a7be874a622646448bc3d21fc2dd0
DhirajSurvase <dms25121997@gmail.com>
2018-02-09 17:39:45 +0530

    Panelli-vendor

--- panelli-vendor.mk
+++ panelli-vendor.mk
@@ -1,19 +1,2 @@
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/motorola/panelli/bin,system/bin)
-
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/motorola/panelli/etc,system/etc)
-
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/motorola/panelli/lib,system/lib)
-
-#PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/motorola/panelli/priv-app,system/priv-app)
-
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/motorola/panelli/usr,system/usr)
-
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/motorola/panelli/vendor/bin,system/vendor/bin)
-
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/motorola/panelli/vendor/etc,system/vendor/etc)
-
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/motorola/panelli/vendor/firmware,system/vendor/firmware)
-
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/motorola/panelli/vendor/lib,system/vendor/lib)
-
-PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/motorola/panelli/vendor/thh,system/vendor/thh)
+#Vendor
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/moto/panelli/proprietary,system)
