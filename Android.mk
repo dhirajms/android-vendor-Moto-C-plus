387b2530ea42348a12080c3a3e2b9041343995b0
DhirajMS137 <dms25121997@gmail.com>
2018-02-09 16:35:40 +0530

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -25,14 +25,14 @@ LOCAL_MULTILIB = 32
 LOCAL_SRC_FILES_32 = vendor/lib/libdpframework.so
 include $(BUILD_PREBUILT)
  
-#include $(CLEAR_VARS)
-#LOCAL_MODULE = libstagefright_color_conversion
-#LOCAL_MODULE_CLASS = STATIC_LIBRARIES
-#LOCAL_MODULE_SUFFIX = .a
-#LOCAL_UNINSTALLABLE_MODULE = true
-#LOCAL_MULTILIB = 32
-#LOCAL_SHARED_LIBRARIES_32 = libdpframework
-#LOCAL_SRC_FILES_32 = libstagefright_color_conversion/libstagefright_color_conversion_32.a
-#include $(BUILD_PREBUILT)
+include $(CLEAR_VARS)
+LOCAL_MODULE = libstagefright_color_conversion
+LOCAL_MODULE_CLASS = STATIC_LIBRARIES
+LOCAL_MODULE_SUFFIX = .a
+LOCAL_UNINSTALLABLE_MODULE = true
+LOCAL_MULTILIB = 32
+LOCAL_SHARED_LIBRARIES_32 = libdpframework
+LOCAL_SRC_FILES_32 = libstagefright_color_conversion/libstagefright_color_conversion_32.a
+include $(BUILD_PREBUILT)
 
 endif
