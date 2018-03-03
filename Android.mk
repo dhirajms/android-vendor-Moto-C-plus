cfd00b3a037176dbc403623dbb63a4f6e8adc786
DhirajSurvase <dms25121997@gmail.com>
2018-03-03 22:26:11 +0000

    update vendor blobs

--- Android.mk
+++ Android.mk
@@ -1,39 +1,20 @@
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter panelli, $(TARGET_DEVICE)),)
+include $(CLEAR_VARS)
+LOCAL_MODULE := libdpframework
+LOCAL_SRC_FILES := proprietary/lib/libdpframework.so
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_SUFFIX := .so
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)
+include $(BUILD_PREBUILT)
+
 include $(CLEAR_VARS)
 LOCAL_MODULE := libaudiocustparam
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_SRC_FILES := proprietary/lib/libaudiocustparam.so
+LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_SUFFIX := .so
-LOCAL_MULTILIB := 32
-LOCAL_SRC_FILES_32 := proprietary/vendor/lib/libaudiocustparam.so
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)
 include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE = libmtkcam_fwkutils
-LOCAL_MODULE_CLASS = SHARED_LIBRARIES
-LOCAL_MODULE_SUFFIX = .so
-LOCAL_MULTILIB = 32
-LOCAL_SRC_FILES_32 = proprietary/vendor/lib/libmtkcam_fwkutils.so
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE = libdpframework
-LOCAL_MODULE_CLASS = SHARED_LIBRARIES
-LOCAL_MODULE_SUFFIX = .so
-LOCAL_MULTILIB = 32
-LOCAL_SRC_FILES_32 = proprietary/vendor/lib/libdpframework.so
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE = libstagefright_color_conversion
-LOCAL_MODULE_CLASS = STATIC_LIBRARIES
-LOCAL_MODULE_SUFFIX = .a
-LOCAL_UNINSTALLABLE_MODULE = true
-LOCAL_MULTILIB = 32
-LOCAL_SHARED_LIBRARIES_32 = libdpframework
-LOCAL_SRC_FILES_32 = libstagefright_color_conversion_32.a
-include $(BUILD_PREBUILT)
-
-endif
 
